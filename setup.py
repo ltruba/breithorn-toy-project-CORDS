7e725f2e72b539f820bcd69c24c02d77d794e484
leobont <leo.bont@wsl.ch>
2024-06-10 16:18:44 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 16:05:49 2024
+
+@author: BontL
+"""
+
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+)
